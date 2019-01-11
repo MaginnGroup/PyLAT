1dac956269d27337f14c8df5c2c7b8e102b12cba
Michael Humbert <mike5603@comcast.net>
2019-01-11 10:58:34 -0500

    quotation fix

--- src/viscio.py
+++ src/viscio.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''
+"""
 
 import re
 import numpy as np
