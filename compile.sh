8cd5bb003c2db7fb3622cfbc4b656612d802d5ab
Michael Humbert <mike5603@comcast.net>
2019-11-19 13:12:20 -0600

    update compile.sh to hopefully fix loading issues

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 cd src
-f2py -c -m calccomf calcCOM.f90
-f2py -c -m calcdistances calcdistances.f90
-f2py -c -m ipcorr ipcorr.f90
+python -m numpy.f2py -c -m calccomf calcCOM.f90
+python -m numpy.f2py -c -m calcdistances calcdistances.f90
+python -m numpy.f2py -c -m ipcorr ipcorr.f90
 #f2py -c -m src/elradial src/elradial.f90
 #f2py -c -m src/siteradial src/siteradial.f90
 cd ..
