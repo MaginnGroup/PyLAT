8ed488a2f3b4ff7fe46ef8f28ec2743be92a4bb9
Amrita Goswami <amrita16thaug646@gmail.com>
2020-08-12 00:58:39 +0530

    viscio.py: Fixed type error in slice index

--- src/viscio.py
+++ src/viscio.py
@@ -34,7 +34,7 @@ def _list2float(seq):
 def autocorrelate (a):
     b=np.concatenate((a,np.zeros(len(a))),axis=0)
     c= np.fft.ifft(np.fft.fft(b)*np.conjugate(np.fft.fft(b))).real
-    d=c[:len(c)/2]
+    d=c[:int(len(c)/2)]
     d=d/(np.array(range(len(a)))+1)[::-1]
     return d
 
